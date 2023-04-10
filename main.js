f6ae836697946cb2b8ebfea4a33edf1eff40a488
LilianBernot <lilian.bernot@gmail.com>
2023-04-10 16:20:39 -0400

    modif des titres

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function toggleShowList(button, to_modify) {
-    const actionButton = document.getElementById(button);
-    const element = document.getElementById(to_modify);
-    const isHidden = element.style.display === "none";
-    if (isHidden) {
-        element.style.display = "block";
-        actionButton.innerHTML = "Fermer étape";
-    } else {
-        element.style.display = "none";
-        actionButton.innerHTML = "Ouvrir étape";
-    }
+    // const actionButton = document.getElementById(button);
+    // const element = document.getElementById(to_modify);
+    // const isHidden = element.style.display === "none";
+    // if (isHidden) {
+    //     element.style.display = "block";
+    //     // actionButton.innerHTML = "Fermer étape";
+    // } else {
+    //     element.style.display = "none";
+    //     // actionButton.innerHTML = "Ouvrir étape";
+    // }
   };
